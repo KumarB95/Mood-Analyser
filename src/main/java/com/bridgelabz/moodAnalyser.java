ceeb8c4626176a109afe8448de9ea7c4bd57fd15
KumarB95 <kumarbalingal@gmail.com>
2021-10-18 11:57:11 +0530

    Refactored : Analyse and respond Sad or Happy to given message

--- src/main/java/com/bridgelabz/moodAnalyser.java
+++ src/main/java/com/bridgelabz/moodAnalyser.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
-
+/*
+* Mood analyser program
+* Author : Navalkumar
+* Date : 18/10/21
+* */
 public class moodAnalyser {
     public static void main(String[] args) {
         System.out.println("Welcome to mood analyser Program");
+    }
+
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -4,4 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class MoodAnalyserTest {
+    @Test
+    void givenMessage_WhenSad_ShouldReturnSad() {
+        moodAnalyser moodAnalyser = new moodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        Assertions.assertEquals("SAD", mood);
+    }
 }
